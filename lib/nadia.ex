446599ba1dc047ca0e55be92b2d4ab6a77cd4037
zhyu <angellwings@gmail.com>
2015-08-17 02:26:41 +0900

    methods to build api urls

--- lib/nadia.ex
+++ lib/nadia.ex
@@ -1,2 +1,9 @@
 defmodule Nadia do
+
+  @base_url "https://api.telegram.org/bot"
+
+  defp token, do: Application.get_env(:nadia, :token)
+
+  defp build_url(method), do: @base_url <> token <> "/" <> method
+
 end

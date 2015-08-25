17b2eca0145e16628abbb505cbc0d80636b571b7
zhyu <angellwings@gmail.com>
2015-08-26 00:25:25 +0900

    add documents for API args

--- lib/nadia/api.ex
+++ lib/nadia/api.ex
@@ -69,6 +69,16 @@ defmodule Nadia.API do
       @doc """
       Use this method to send text messages.
       On success, the sent Message is returned.
+
+      Args:
+      * `chat_id` - Unique identifier for the message recipient — `Nadia.User `or `Nadia.GroupChat` id
+      * `text` - Text of the message to be sent
+      * `options` - orddict of options
+
+      Options:
+      * `:disable_web_page_preview` - Disables link previews for links in this message
+      * `:reply_to_message_id` - If the message is a reply, ID of the original message
+      * `:reply_markup` - Additional interface options. Instructions to hide keyboard or to force a reply from the user.
       """
       @spec send_message(integer, binary, [{atom, any}]) :: {:ok, Message.t} | {:error, Error.t}
       def send_message(chat_id, text, options \\ []) do
@@ -79,6 +89,11 @@ defmodule Nadia.API do
       @doc """
       Use this method to forward messages of any kind.
       On success, the sent Message is returned.
+
+      Args:
+      * `chat_id` - Unique identifier for the message recipient — `Nadia.User` or `Nadia.GroupChat` id
+      * `from_chat_id` - Unique identifier for the chat where the original message was sent — `Nadia.User `or `Nadia.GroupChat` id
+      * `message_id` - Unique message identifier
       """
       @spec forward_message(integer, integer, integer) :: {:ok, Message.t} | {:error, Error.t}
       def forward_message(chat_id, from_chat_id, message_id) do
@@ -89,6 +104,17 @@ defmodule Nadia.API do
       @doc """
       Use this method to send photos.
       On success, the sent Message is returned.
+
+      Args:
+      * `chat_id` - Unique identifier for the message recipient — `Nadia.User `or `Nadia.GroupChat` id
+      * `photo` - Photo to send. Either a `file_id` to resend a photo that is already on the Telegram servers,
+      or a `file_path` to upload a new photo
+      * `options` - orddict of options
+
+      Options:
+      * `:caption` - Photo caption (may also be used when resending photos by `file_id`)
+      * `:reply_to_message_id` - If the message is a reply, ID of the original message
+      * `:reply_markup` - Additional interface options. Instructions to hide keyboard or to force a reply from the user
       """
       @spec send_photo(integer, binary, [{atom, any}]) :: {:ok, Message.t} | {:error, Error.t}
       def send_photo(chat_id, photo, options \\ []) do
@@ -108,6 +134,19 @@ defmodule Nadia.API do
       will be sent as a playable voice message. For this to work, the audio must be
       in an .ogg file encoded with OPUS. This behavior will be phased out in the
       future. For sending voice messages, use the sendVoice method instead.
+
+      Args:
+      * `chat_id` - Unique identifier for the message recipient — `Nadia.User `or `Nadia.GroupChat` id
+      * `audio` - Audio to send. Either a `file_id` to resend an audio that is already on the Telegram servers,
+      or a `file_path` to upload a new audio
+      * `options` - orddict of options
+
+      Options:
+      * `:duration` - Duration of the audio in seconds
+      * `:performer` - Performer
+      * `:title` - Track name
+      * `:reply_to_message_id` - If the message is a reply, ID of the original message
+      * `:reply_markup` - Additional interface options. Instructions to hide keyboard or to force a reply from the user
       """
       @spec send_audio(integer, binary, [{atom, any}]) :: {:ok, Message.t} | {:error, Error.t}
       def send_audio(chat_id, audio, options \\ []) do
@@ -120,6 +159,16 @@ defmodule Nadia.API do
       On success, the sent Message is returned.
       Bots can currently send files of any type of up to 50 MB in size, this limit
       may be changed in the future.
+
+      Args:
+      * `chat_id` - Unique identifier for the message recipient — `Nadia.User `or `Nadia.GroupChat` id
+      * `document` - File to send. Either a `file_id` to resend a file that is already on the Telegram servers,
+      or a `file_path` to upload a new file
+      * `options` - orddict of options
+
+      Options:
+      * `:reply_to_message_id` - If the message is a reply, ID of the original message
+      * `:reply_markup` - Additional interface options. Instructions to hide keyboard or to force a reply from the user
       """
       @spec send_document(integer, binary, [{atom, any}]) :: {:ok, Message.t} | {:error, Error.t}
       def send_document(chat_id, document, options \\ []) do
@@ -130,6 +179,16 @@ defmodule Nadia.API do
       @doc """
       Use this method to send .webp stickers.
       On success, the sent Message is returned.
+
+      Args:
+      * `chat_id` - Unique identifier for the message recipient — `Nadia.User `or `Nadia.GroupChat` id
+      * `sticker` - File to send. Either a `file_id` to resend a sticker that is already on the Telegram servers,
+      or a `file_path` to upload a new sticker
+      * `options` - orddict of options
+
+      Options:
+      * `:reply_to_message_id` - If the message is a reply, ID of the original message
+      * `:reply_markup` - Additional interface options. Instructions to hide keyboard or to force a reply from the user
       """
       @spec send_sticker(integer, binary, [{atom, any}]) :: {:ok, Message.t} | {:error, Error.t}
       def send_sticker(chat_id, sticker, options \\ []) do
@@ -143,6 +202,18 @@ defmodule Nadia.API do
       On success, the sent Message is returned.
       Bots can currently send video files of up to 50 MB in size, this limit may be
       changed in the future.
+
+      Args:
+      * `chat_id` - Unique identifier for the message recipient — `Nadia.User `or `Nadia.GroupChat` id
+      * `video` - Video to send. Either a `file_id` to resend a video that is already on the Telegram servers,
+      or a `file_path` to upload a new video
+      * `options` - orddict of options
+
+      Options:
+      * `:duration` - Duration of the video in seconds
+      * `:caption` - Video caption (may also be used when resending videos by `file_id`)
+      * `:reply_to_message_id` - If the message is a reply, ID of the original message
+      * `:reply_markup` - Additional interface options. Instructions to hide keyboard or to force a reply from the user
       """
       @spec send_video(integer, binary, [{atom, any}]) :: {:ok, Message.t} | {:error, Error.t}
       def send_video(chat_id, video, options \\ []) do
@@ -157,6 +228,17 @@ defmodule Nadia.API do
       On success, the sent Message is returned.
       Bots can currently send voice messages of up to 50 MB in size, this limit may be
       changed in the future.
+
+      Args:
+      * `chat_id` - Unique identifier for the message recipient — `Nadia.User `or `Nadia.GroupChat` id
+      * `voice` - Audio to send. Either a `file_id` to resend an audio that is already on the Telegram servers,
+      or a `file_path` to upload a new audio
+      * `options` - orddict of options
+
+      Options:
+      * `:duration` - Duration of the audio in seconds
+      * `:reply_to_message_id` - If the message is a reply, ID of the original message
+      * `:reply_markup` - Additional interface options. Instructions to hide keyboard or to force a reply from the user
       """
       @spec send_voice(integer, binary, [{atom, any}]) :: {:ok, Message.t} | {:error, Error.t}
       def send_voice(chat_id, voice, options \\ []) do
@@ -167,6 +249,16 @@ defmodule Nadia.API do
       @doc """
       Use this method to send point on the map.
       On success, the sent Message is returned.
+
+      Args:
+      * `chat_id` - Unique identifier for the message recipient — `Nadia.User `or `Nadia.GroupChat` id
+      * `latitude` - Latitude of location
+      * `longitude` - Longitude of location
+      * `options` - orddict of options
+
+      Options:
+      * `:reply_to_message_id` - If the message is a reply, ID of the original message
+      * `:reply_markup` - Additional interface options. Instructions to hide keyboard or to force a reply from the user
       """
       @spec send_location(integer, float, float, [{atom, any}]) :: {:ok, Message.t} | {:error, Error.t}
       def send_location(chat_id, latitude, longitude, options \\ []) do
@@ -178,6 +270,16 @@ defmodule Nadia.API do
       Use this method when you need to tell the user that something is happening on
       the bot's side. The status is set for 5 seconds or less (when a message
       arrives from your bot, Telegram clients clear its typing status).
+
+      Args:
+      * `chat_id` - Unique identifier for the message recipient — `Nadia.User `or `Nadia.GroupChat` id
+      * `action` - Type of action to broadcast. Choose one, depending on what the user is about to receive:
+          * `typing` for text messages
+          * `upload_photo` for photos
+          * `record_video` or `upload_video` for videos
+          * `record_audio` or `upload_audio` for audio files
+          * `upload_document` for general files
+          * `find_location` for location data
       """
       @spec send_chat_action(integer, binary) :: :ok | {:error, Error.t}
       def send_chat_action(chat_id, action) do
@@ -188,6 +290,14 @@ defmodule Nadia.API do
       @doc """
       Use this method to get a list of profile pictures for a user.
       Returns a UserProfilePhotos object.
+
+      Args:
+      * `user_id` - Unique identifier of the target user
+      * `options` - orddict of options
+
+      Options:
+      * `:offset` - Sequential number of the first photo to be returned. By default, all photos are returned
+      * `:limit` - Limits the number of photos to be retrieved. Values between 1—100 are accepted. Defaults to 100
       """
       @spec get_user_profile_photos(integer, [{atom, any}]) :: {:ok, UserProfilePhotos.t} | {:error, Error.t}
       def get_user_profile_photos(user_id, options \\ []) do
@@ -198,6 +308,17 @@ defmodule Nadia.API do
       @doc """
       Use this method to receive incoming updates using long polling.
       An Array of Update objects is returned.
+
+      Args:
+      * `options` - orddict of options
+
+      Options:
+      * `:offset` - Identifier of the first update to be returned. Must be greater by one than the highest
+      among the identifiers of previously received updates. By default, updates starting with the earliest
+      unconfirmed update are returned. An update is considered confirmed as soon as `get_updates` is called
+      with an `offset` higher than its `update_id`.
+      * `:limit` - Limits the number of photos to be retrieved. Values between 1—100 are accepted. Defaults to 100
+      * `:timeout` - Timeout in seconds for long polling. Defaults to 0, i.e. usual short polling
       """
       @spec get_updates([{atom, any}]) :: {:ok, [Update.t]} | {:error, Error.t}
       def get_updates(options \\ []) do
@@ -210,6 +331,12 @@ defmodule Nadia.API do
       webhook. Whenever there is an update for the bot, we will send an HTTPS POST
       request to the specified url, containing a JSON-serialized Update. In case of
       an unsuccessful request, we will give up after a reasonable amount of attempts.
+
+      Args:
+      * `options` - orddict of options
+
+      Options:
+      * `:url` - HTTPS url to send updates to. Use an empty string to remove webhook integration
       """
       @spec set_webhook([{atom, any}]) :: :ok | {:error, Error.t}
       def set_webhook(options \\ []) do

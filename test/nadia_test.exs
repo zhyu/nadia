524d2908ba02a2985bbc27b36042f21ef72c9b3e
zhyu <angellwings@gmail.com>
2015-08-31 10:14:20 +0900

    record actual HTTP response to test

--- test/nadia_test.exs
+++ test/nadia_test.exs
@@ -3,30 +3,23 @@ defmodule NadiaTest do
   use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
 
   alias Nadia.User
-  alias Nadia.Message
-  alias Nadia.Update
 
   setup_all do
-    Application.put_env(:nadia, :token, "TEST_TOKEN")
+    ExVCR.Config.filter_sensitive_data("bot[^/]+/", "bot<TOKEN>/")
+    :ok
   end
 
   test "get_me" do
     use_cassette "get_me" do
       {:ok, me} = Nadia.get_me
-      assert me == %User{id: 666, first_name: "Nadia", username: "nadia_bot"}
+      assert me == %User{id: 81420469, first_name: "Nadia", username: "nadia_bot"}
     end
   end
 
   test "get_updates" do
     use_cassette "get_updates" do
-      {:ok, updates} = Nadia.get_updates
+      {:ok, updates} = Nadia.get_updates(limit: 1)
       assert length(updates) == 1
-
-      user = %User{id: 777, first_name: "Tester", username: "tester"}
-      assert hd(updates) == %Update{
-        update_id: 1,
-        message: %Message{
-          chat: user, from: user, date: 1440926322, message_id: 1, text: "test"}}
     end
   end
 end

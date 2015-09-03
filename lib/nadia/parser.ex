95db22935745cf122217ec177da963b7943dbcbc
zhyu <angellwings@gmail.com>
2015-09-03 12:36:14 +0900

    parse by recursion

--- lib/nadia/parser.ex
+++ lib/nadia/parser.ex
@@ -22,101 +22,40 @@ defmodule Nadia.Parser do
   """
   def parse_result(result, method) do
     case method do
-      "getMe" -> struct(User, result)
+      "getMe" -> parse(User, result)
       "sendChatAction" -> result
-      "getUserProfilePhotos" -> parse_user_profile_photos(result)
-      "getUpdates" -> parse_updates(result)
+      "getUserProfilePhotos" -> parse(UserProfilePhotos, result)
+      "getUpdates" -> parse(:updates, result)
       "setWebhook" -> result
-      _ -> parse_message(result)
+      _ -> parse(Message, result)
     end
   end
 
-  defp parse_message(nil), do: nil
-  defp parse_message(message) do
-    message = struct(Message, message)
-    from = struct(User, message.from)
-    chat = if Dict.has_key?(message.chat, :title) do
-      struct(GroupChat, message.chat)
-    else
-      struct(User, message.chat)
-    end
-    message = %{message | from: from, chat: chat}
+  @keys_of_message [:message, :reply_to_message]
+  @keys_of_photo [:photo, :new_chat_photo]
+  @keys_of_user [:from, :forward_from, :new_chat_participant, :left_chat_participant]
 
-    if message.forward_from do
-      message = %{message | forward_from: struct(User, message.forward_from)}
-    end
-    if message.reply_to_message do
-      message = %{message | reply_to_message: parse_message(message.reply_to_message)}
-    end
-    if message.audio do
-      message = %{message | audio: struct(Audio, message.audio)}
-    end
-    if message.document do
-      message = %{message | document: parse_document(message.document)}
-    end
-    if message.photo do
-      photo = for photo_size <- message.photo, do: struct(PhotoSize, photo_size)
-      message = %{message | photo: photo}
-    end
-    if message.sticker do
-      message = %{message | sticker: parse_sticker(message.sticker)}
-    end
-    if message.video do
-      message = %{message | video: parse_video(message.video)}
-    end
-    if message.voice do
-      message = %{message | voice: struct(Voice, message.voice)}
-    end
-    if message.contact do
-      message = %{message | contact: struct(Contact, message.contact)}
-    end
-    if message.location do
-      message = %{message | location: struct(Location, message.location)}
-    end
-    if message.new_chat_participant do
-      message = %{message | new_chat_participant: struct(User, message.new_chat_participant)}
-    end
-    if message.left_chat_participant do
-      message = %{message | left_chat_participant: struct(User, message.left_chat_participant)}
-    end
-    if message.new_chat_photo do
-      new_chat_photo = for photo_size <- message.new_chat_photo, do: struct(PhotoSize, photo_size)
-      message = %{message | new_chat_photo: new_chat_photo}
-    end
+  defp chat_type(%{title: _}), do: GroupChat
+  defp chat_type(_), do: User
 
-    message
-  end
-
-  defp parse_photo_size(nil), do: nil
-  defp parse_photo_size(photo_size), do: struct(PhotoSize, photo_size)
-
-  defp parse_document(document) do
-    document = struct(Document, document)
-    %{document | thumb: parse_photo_size(document.thumb)}
-  end
-
-  defp parse_sticker(sticker) do
-    sticker = struct(Sticker, sticker)
-    %{sticker | thumb: parse_photo_size(sticker.thumb)}
-  end
-
-  defp parse_video(video) do
-    video = struct(Video, video)
-    %{video | thumb: parse_photo_size(video.thumb)}
-  end
-
-  defp parse_user_profile_photos(user_profile_photos) do
-    user_profile_photos = struct(UserProfilePhotos, user_profile_photos)
-    photos = for photo <- user_profile_photos.photos do
-      for photo_size <- photo, do: struct(PhotoSize, photo_size)
-    end
-    %{user_profile_photos | photos: photos}
-  end
-
-  defp parse_updates(updates) do
-    for update <- updates do
-      update = struct(Update, update)
-      %{update | message: parse_message(update.message)}
-    end
-  end
+  defp parse(_, nil), do: nil
+  defp parse(:photo, l) when is_list(l), do: Enum.map(l, &(parse(PhotoSize, &1)))
+  defp parse(:photos, l) when is_list(l), do: Enum.map(l, &(parse(:photo, &1)))
+  defp parse(:updates, l) when is_list(l), do: Enum.map(l, &(parse(Update, &1)))
+  defp parse(type, val), do: struct(type, Enum.map(val, &(parse(&1))))
+  defp parse({key, nil}), do: {key, nil}
+  defp parse({:chat, val}), do: {:chat, parse(chat_type(val), val)}
+  defp parse({:audio, val}), do: {:audio, parse(Audio, val)}
+  defp parse({:video, val}), do: {:video, parse(Video, val)}
+  defp parse({:voice, val}), do: {:voice, parse(Voice, val)}
+  defp parse({:sticker, val}), do: {:sticker, parse(Sticker, val)}
+  defp parse({:document, val}), do: {:document, parse(Document, val)}
+  defp parse({:contact, val}), do: {:contact, parse(Contact, val)}
+  defp parse({:location, val}), do: {:location, parse(Location, val)}
+  defp parse({:thumb, val}), do: {:thumb, parse(PhotoSize, val)}
+  defp parse({:photos, val}), do: {:photos, parse(:photos, val)}
+  defp parse({key, val}) when key in @keys_of_photo, do: {key, parse(:photo, val)}
+  defp parse({key, val}) when key in @keys_of_user, do: {key, parse(User, val)}
+  defp parse({key, val}) when key in @keys_of_message, do: {key, parse(Message, val)}
+  defp parse(others), do: others
 end

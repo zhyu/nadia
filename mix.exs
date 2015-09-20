63b6d93143bf6fda913ec96080664a3bf2f8191a
zhyu <angellwings@gmail.com>
2015-09-20 23:26:14 +0900

    bump version to 0.3.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Nadia.Mixfile do
 
   def project do
     [app: :nadia,
-     version: "0.3.1",
+     version: "0.3.2",
      elixir: "~> 1.0",
      description: "Telegram Bot API Wrapper written in Elixir",
      package: package,

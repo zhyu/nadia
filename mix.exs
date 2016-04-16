075cb730cd096819d81e7a71592a2db1bf39b944
Col Harris <col.w.harris@gmail.com>
2016-04-17 03:45:46 +0800

    Bump poison to 2.0

--- mix.exs
+++ mix.exs
@@ -30,11 +30,11 @@ defmodule Nadia.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [{:httpoison, "~> 0.7"},
-     {:poison, "~> 1.5.0"},
+     {:poison, "~> 2.0"},
      {:exvcr, "~> 0.5.0", only: [:dev, :test]},
      {:earmark, "~> 0.1", only: :docs},
      {:ex_doc, "~> 0.9", only: :docs},
-     {:inch_ex, only: :docs}]
+     {:inch_ex, "~> 0.5.1", only: :docs}]
   end
 
   defp package do

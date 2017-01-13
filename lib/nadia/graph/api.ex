9a639c07619bcf52dda8149baf1427d951d1ee80
korchasa <korchasa@gmail.com>
2017-01-13 04:41:16 +0200

    telegra.ph API

---
+++ lib/nadia/graph/api.ex
@@ -0,0 +1,78 @@
+defmodule Nadia.Graph.API do
+  @moduledoc """
+  Provides basic functionalities for Telegram Bot API.
+  """
+
+  alias Nadia.Graph.Model.Error
+
+  @default_timeout 5
+  @base_url "https://api.telegra.ph"
+
+  defp recv_timeout, do: config_or_env(:recv_timeout) || @default_timeout
+
+  defp config_or_env(key) do
+    case Application.fetch_env(:nadia, key) do
+      {:ok, {:system, var}} -> System.get_env(var)
+      {:ok, {:system, var, default}} ->
+        case System.get_env(var) do
+          nil -> default
+          val -> val
+        end
+      {:ok, value} -> value
+      :error -> nil
+    end
+  end
+
+  defp build_url(method), do: @base_url <> "/" <> method
+
+  defp process_response(response, method) do
+    case decode_response(response) do
+      {:ok, true} -> :ok
+      {:ok, result} -> {:ok, Nadia.Graph.Parser.parse_result(result, method)}
+      %{ok: false, description: description} -> {:error, %Error{reason: description}}
+      {:error, %HTTPoison.Error{reason: reason}} -> {:error, %Error{reason: reason}}
+    end
+  end
+
+  defp decode_response(response) do
+    with {:ok, %HTTPoison.Response{body: body}} <- response,
+          %{result: result} <- Poison.decode!(body, keys: :atoms),
+      do: {:ok, result}
+  end
+
+  defp build_multipart_request(params, file_field) do
+    {file_path, params} = Keyword.pop(params, file_field)
+    params = for {k, v} <- params, do: {to_string(k), v}
+    {:multipart, params ++ [
+      {:file, file_path,
+       {"form-data", [{"name", to_string(file_field)}, {"filename", file_path}]}, []}
+    ]}
+  end
+
+  defp build_request(params, file_field) do
+    params = params
+    |> Keyword.update(:reply_markup, nil, &(Poison.encode!(&1)))
+    |> Enum.filter_map(fn {_, v} -> v end, fn {k, v} -> {k, to_string(v)} end)
+    if !is_nil(file_field) and File.exists?(params[file_field]) do
+      build_multipart_request(params, file_field)
+    else
+      {:form, params}
+    end
+  end
+
+  @doc """
+  Generic method to call Telegram Bot API.
+
+  Args:
+  * `method` - name of API method
+  * `options` - orddict of options
+  * `file_field` - specify the key of file_field in `options` when sending files
+  """
+  def request(method, options \\ [], file_field \\ nil) do
+    timeout = (Keyword.get(options, :timeout, 0) + recv_timeout()) * 1000
+    method
+    |> build_url
+    |> HTTPoison.post(build_request(options, file_field), [], recv_timeout: timeout)
+    |> process_response(method)
+  end
+end
---
+++ test/nadia/graph_test.exs
@@ -0,0 +1,96 @@
+defmodule Nadia.GraphTest do
+  use ExUnit.Case
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
+  doctest Nadia.Graph
+
+  setup_all do
+    ExVCR.Config.filter_sensitive_data("bot[^/]+/", "bot<TOKEN>/")
+    ExVCR.Config.filter_sensitive_data("id\":\\d+", "id\":666")
+    ExVCR.Config.filter_sensitive_data("id=\\d+", "id=666")
+    ExVCR.Config.filter_sensitive_data("_id=@w+", "_id=@group")
+    :ok
+  end
+
+  test "create_account" do
+    use_cassette "graph/create_account" do
+      {:ok, account} = Nadia.Graph.create_account("some_short_name", "some_author_name")
+      assert account.short_name == "some_short_name"
+    end
+  end
+
+  test "edit_account_info" do
+    use_cassette "graph/edit_account_info" do
+      {:ok, account} = Nadia.Graph.edit_account_info(
+        "b968da509bb76866c35425099bc0989a5ec3b32997d55286c657e6994bbb",
+        "some_short_name",
+        "some_author_name"
+        )
+      assert account.short_name == "some_short_name"
+    end
+  end
+
+  test "get_account_info" do
+    use_cassette "graph/get_account_info" do
+      {:ok, account} = Nadia.Graph.get_account_info(
+        "b968da509bb76866c35425099bc0989a5ec3b32997d55286c657e6994bbb",
+        ["short_name", "page_count"]
+        )
+      assert account.short_name == "some_short_name"
+    end
+  end
+
+  test "revoke_access_token" do
+    use_cassette "graph/revoke_access_token" do
+      {:ok, account} = Nadia.Graph.revoke_access_token(
+        "b968da509bb76866c35425099bc0989a5ec3b32997d55286c657e6994bbb"
+        )
+      assert account.short_name == "some_short_name"
+    end
+  end
+
+  test "get_page_list" do
+    use_cassette "graph/get_page_list" do
+      {:ok, page_list} = Nadia.Graph.get_page_list("b968da509bb76866c35425099bc0989a5ec3b32997d55286c657e6994bbb" , 0, 3)
+      assert page_list.total_count == 1058
+      assert hd(page_list.pages).path == "Sample-Page-01-13-6"
+    end
+  end
+
+  test "create_page" do
+    use_cassette "graph/create_page" do
+      {:ok, page} = Nadia.Graph.create_page(
+        "b968da509bb76866c35425099bc0989a5ec3b32997d55286c657e6994bbb",
+        "Sample Page",
+        "[{\"tag\":\"p\",\"children\":[\"Hello,+world!\"]}]"
+      )
+      assert page.title == "Sample Page"
+    end
+  end
+
+  test "get_page" do
+    use_cassette "graph/get_page" do
+      {:ok, page} = Nadia.Graph.get_page("Sample-Page-12-15")
+      assert page.title == "Sample Page"
+      assert page.content != nil
+    end
+  end
+
+  test "edit_page" do
+    use_cassette "graph/edit_page" do
+      {:ok, page} = Nadia.Graph.edit_page(
+        "b968da509bb76866c35425099bc0989a5ec3b32997d55286c657e6994bbb",
+        "Sample-Page-12-15",
+        "Some Page2",
+        "[{\"tag\":\"p\",\"children\":[\"Hello,+world!\"]}]"
+      )
+      assert page.title == "Sample Page2"
+    end
+  end
+
+   test "get_views" do
+    use_cassette "graph/get_views" do
+      {:ok, result} = Nadia.Graph.get_views("Sample-Page-12-15", [year: 2012, month: 12])
+      assert result.views == 40
+    end
+  end
+end

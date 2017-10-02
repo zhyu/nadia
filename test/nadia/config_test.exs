99316021a48d0b1dce55400fbdcba216c3b03ef5
Boris Bera <bera.boris@gmail.com>
2017-10-02 00:01:18 -0400

    Test graph_base_url/0

--- test/nadia/config_test.exs
+++ test/nadia/config_test.exs
@@ -29,4 +29,32 @@ defmodule Nadia.ConfigTest do
       assert Config.base_url() == "https://api.telegram.org/bot"
     end
   end
+
+  describe "graph_base_url/0" do
+    test "returns config value when present" do
+      :ok = Application.put_env(:nadia, :graph_base_url, "http://something.com/api")
+
+      assert Config.graph_base_url() == "http://something.com/api"
+    end
+
+    test "returns environment variable" do
+      :ok = Application.put_env(:nadia, :graph_base_url, {:system, "PHONY_BASE_URL"})
+      :ok = System.put_env("PHONY_BASE_URL", "http://somethingelse.com/api")
+
+      assert Config.graph_base_url() == "http://somethingelse.com/api"
+    end
+
+    test "returns environment variable default" do
+      :ok = Application.put_env(:nadia, :graph_base_url,
+        {:system, "PHONY_BASE_URL", "http://somedefault.com/api"})
+      :ok = System.delete_env("PHONY_BASE_URL")
+
+      assert Config.graph_base_url() == "http://somedefault.com/api"
+    end
+    test "returns default when unset" do
+      :ok = Application.delete_env(:nadia, :graph_base_url)
+
+      assert Config.graph_base_url() == "https://api.telegra.ph"
+    end
+  end
 end

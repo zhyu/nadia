fe2d36174ea69bb00540d35dcf57a35ce73c0856
Boris Bera <bera.boris@gmail.com>
2017-10-01 23:47:00 -0400

    Test Config.base_url/0

---
+++ test/nadia/config_test.exs
@@ -0,0 +1,32 @@
+defmodule Nadia.ConfigTest do
+  use ExUnit.Case
+  alias Nadia.Config
+
+  describe "base_url/0" do
+    test "returns config value when present" do
+      :ok = Application.put_env(:nadia, :base_url, "http://something.com/api")
+
+      assert Config.base_url() == "http://something.com/api"
+    end
+
+    test "returns environment variable" do
+      :ok = Application.put_env(:nadia, :base_url, {:system, "PHONY_BASE_URL"})
+      :ok = System.put_env("PHONY_BASE_URL", "http://somethingelse.com/api")
+
+      assert Config.base_url() == "http://somethingelse.com/api"
+    end
+
+    test "returns environment variable default" do
+      :ok = Application.put_env(:nadia, :base_url,
+        {:system, "PHONY_BASE_URL", "http://somedefault.com/api"})
+      :ok = System.delete_env("PHONY_BASE_URL")
+
+      assert Config.base_url() == "http://somedefault.com/api"
+    end
+    test "returns default when unset" do
+      :ok = Application.delete_env(:nadia, :base_url)
+
+      assert Config.base_url() == "https://api.telegram.org/bot"
+    end
+  end
+end

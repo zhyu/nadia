d51d2938ae84a0f1fa44b7a227b9c5678255f64c
Boris Bera <bera.boris@gmail.com>
2017-10-02 00:06:27 -0400

    Avoid using ExUnit.Case.describe
    The build runs elixir 1.3 and I guess that describe is just too modern

--- test/nadia/config_test.exs
+++ test/nadia/config_test.exs
@@ -20,59 +20,57 @@ defmodule Nadia.ConfigTest do
     end
   end
 
-  describe "base_url/0" do
-    test "returns config value when present" do
-      :ok = Application.put_env(:nadia, :base_url, "http://something.com/api")
+  test "Config.base_url/0 returns config value when present" do
+    :ok = Application.put_env(:nadia, :base_url, "http://something.com/api")
 
-      assert Config.base_url() == "http://something.com/api"
-    end
-
-    test "returns environment variable" do
-      :ok = Application.put_env(:nadia, :base_url, {:system, "PHONY_BASE_URL"})
-      :ok = System.put_env("PHONY_BASE_URL", "http://somethingelse.com/api")
-
-      assert Config.base_url() == "http://somethingelse.com/api"
-    end
-
-    test "returns environment variable default" do
-      :ok = Application.put_env(:nadia, :base_url,
-        {:system, "PHONY_BASE_URL", "http://somedefault.com/api"})
-      :ok = System.delete_env("PHONY_BASE_URL")
-
-      assert Config.base_url() == "http://somedefault.com/api"
-    end
-    test "returns default when unset" do
-      :ok = Application.delete_env(:nadia, :base_url)
-
-      assert Config.base_url() == "https://api.telegram.org/bot"
-    end
+    assert Config.base_url() == "http://something.com/api"
   end
 
-  describe "graph_base_url/0" do
-    test "returns config value when present" do
-      :ok = Application.put_env(:nadia, :graph_base_url, "http://something.com/api")
+  test "Config.base_url/0 returns environment variable" do
+    :ok = Application.put_env(:nadia, :base_url, {:system, "PHONY_BASE_URL"})
+    :ok = System.put_env("PHONY_BASE_URL", "http://somethingelse.com/api")
 
-      assert Config.graph_base_url() == "http://something.com/api"
-    end
+    assert Config.base_url() == "http://somethingelse.com/api"
+  end
 
-    test "returns environment variable" do
-      :ok = Application.put_env(:nadia, :graph_base_url, {:system, "PHONY_BASE_URL"})
-      :ok = System.put_env("PHONY_BASE_URL", "http://somethingelse.com/api")
+  test "Config.base_url/0 returns environment variable default" do
+    :ok = Application.put_env(:nadia, :base_url,
+      {:system, "PHONY_BASE_URL", "http://somedefault.com/api"})
+    :ok = System.delete_env("PHONY_BASE_URL")
 
-      assert Config.graph_base_url() == "http://somethingelse.com/api"
-    end
+    assert Config.base_url() == "http://somedefault.com/api"
+  end
 
-    test "returns environment variable default" do
-      :ok = Application.put_env(:nadia, :graph_base_url,
-        {:system, "PHONY_BASE_URL", "http://somedefault.com/api"})
-      :ok = System.delete_env("PHONY_BASE_URL")
+  test "Config.base_url/0 returns default when unset" do
+    :ok = Application.delete_env(:nadia, :base_url)
 
-      assert Config.graph_base_url() == "http://somedefault.com/api"
-    end
-    test "returns default when unset" do
-      :ok = Application.delete_env(:nadia, :graph_base_url)
+    assert Config.base_url() == "https://api.telegram.org/bot"
+  end
 
-      assert Config.graph_base_url() == "https://api.telegra.ph"
-    end
+  test "Config.graph_base_url/0 returns config value when present" do
+    :ok = Application.put_env(:nadia, :graph_base_url, "http://something.com/api")
+
+    assert Config.graph_base_url() == "http://something.com/api"
+  end
+
+  test "Config.graph_base_url/0 returns environment variable" do
+    :ok = Application.put_env(:nadia, :graph_base_url, {:system, "PHONY_BASE_URL"})
+    :ok = System.put_env("PHONY_BASE_URL", "http://somethingelse.com/api")
+
+    assert Config.graph_base_url() == "http://somethingelse.com/api"
+  end
+
+  test "Config.graph_base_url/0 returns environment variable default" do
+    :ok = Application.put_env(:nadia, :graph_base_url,
+      {:system, "PHONY_BASE_URL", "http://somedefault.com/api"})
+    :ok = System.delete_env("PHONY_BASE_URL")
+
+    assert Config.graph_base_url() == "http://somedefault.com/api"
+  end
+
+  test "Config.graph_base_url/0 returns default when unset" do
+    :ok = Application.delete_env(:nadia, :graph_base_url)
+
+    assert Config.graph_base_url() == "https://api.telegra.ph"
   end
 end

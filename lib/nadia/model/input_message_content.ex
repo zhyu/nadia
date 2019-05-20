b1ee6c39823a366f1236e5ccdbaa9525c689b692
Shou Ya <shouya@users.noreply.github.com>
2019-05-21 01:15:14 +0800

    enable json encoder for input_message_content

--- lib/nadia/model/input_message_content.ex
+++ lib/nadia/model/input_message_content.ex
@@ -6,16 +6,19 @@ defmodule Nadia.Model.InputMessageContent do
           | Nadia.Model.InputMessageContent.Contact.t()
 
   defmodule Text do
+    @derive Jason.Encoder
     defstruct message_text: nil, parse_mode: nil, disable_web_page_preview: false
     @type t :: %Text{message_text: binary, parse_mode: binary, disable_web_page_preview: atom}
   end
 
   defmodule Location do
+    @derive Jason.Encoder
     defstruct latitude: nil, longitude: nil
     @type t :: %Location{latitude: float, longitude: float}
   end
 
   defmodule Venue do
+    @derive Jason.Encoder
     defstruct latitude: nil, longitude: nil, title: nil, address: nil, foursquare_id: nil
 
     @type t :: %Venue{
@@ -28,6 +31,7 @@ defmodule Nadia.Model.InputMessageContent do
   end
 
   defmodule Contact do
+    @derive Jason.Encoder
     defstruct phone_number: nil, first_name: nil, last_name: nil
     @type t :: %Contact{phone_number: binary, first_name: binary, last_name: binary}
   end

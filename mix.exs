39fc27fd2f2856a6e950bd42ec9ca8fc456dd0cc
zhyu <angellwings@gmail.com>
2020-06-16 01:19:31 +0900

    let mix infer runtime dependency. close #102

--- mix.exs
+++ mix.exs
@@ -14,22 +14,6 @@ defmodule Nadia.Mixfile do
     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
-  def application do
-    [applications: [:httpoison]]
-  end
-
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [
       {:httpoison, "~> 1.7.0"},
